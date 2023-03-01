80bb8b2834a976266be7d556ec43534793332ace
Oldrich Pecak <LoaDy588@seznam.cz>
2023-03-01 18:00:50 +0100

    Switched to CLI11 for argument parsing

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 #include <string>
 #include <omp.h>
-#include "CLI/App.hpp"
-#include "CLI/Formatter.hpp"
-#include "CLI/Config.hpp"
+#include "CLI/CLI.hpp"
 #include "geom.hpp"
 #include "loader.hpp"
 #include "simulator.hpp"
 
 int main(int argc, char* argv[]) {
 
-/*
-    namespace po = boost::program_options;
+    CLI::App app{"Simple particle simulator"};
 
+    std::string config_path;
+    int thread_count = omp_get_max_threads();
 
-     po::options_description desc("---Cube Gamma Scan Simulator---");
-    desc.add_options()
-        ("help,h", "print help message")
-        ("thread_count,t", po::value<int>()->default_value(omp_get_max_threads()), "Max threads to use")
-        ("input_file,i", po::value<std::string>()->required(), "The input json file");
-    po::positional_options_description pd;
-    pd.add("input_file", -1);
+    app.add_option("--input_file, -i, input_file", config_path, "Input json file")->required()->check(CLI::ExistingFile);
+    app.add_option("--thread_count, -t", thread_count, "Max threads to use");
 
-    auto parsed = po::basic_command_line_parser(argc, argv).options(desc).positional(pd).run();
-    po::variables_map vm;
-    po::store(parsed, vm);
+    CLI11_PARSE(app, argc, argv);
 
-    if (vm.contains("help")) {
-        std::cout << desc << std::endl;
-        return EXIT_SUCCESS;
-    }
-    po::notify(vm); */
 
     std::cout << "---Cube Gamma Scan Simulator---" << std::endl;
-    std::cout << "Loading config:" << vm["input_file"].as<std::string>() << std::endl;
-    auto config_json = load_file(vm["input_file"].as<std::string>());
+    std::cout << "Loading config:" << config_path << std::endl;
+    auto config_json = load_file(config_path);
     if (!config_json) {
         std::cerr << "Couldn't load config" << std::endl;
         return EXIT_FAILURE;
@@ -48,8 +35,8 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Loaded experiment: " << config_json->at("experiment_name").get<std::string>() << std::endl;
 
-    std::cout << "Starting simulation with " << vm["thread_count"].as<int>() << " threads" << std::endl;
-    auto data = run_simulation(*sim_env, vm["thread_count"].as<int>());
+    std::cout << "Starting simulation with " << thread_count << " threads" << std::endl;
+    auto data = run_simulation(*sim_env, thread_count);
 
     bool saved = save_data(std::move(data), *sim_env, config_json->at("output_file").get<std::string>());
     if (!saved) {
