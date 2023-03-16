6612d3f0246ee4b4e3939ea73600b16dcd0416db
Oldrich Pecak <LoaDy588@seznam.cz>
2023-03-16 14:28:18 +0100

    Start implementation of volumes

--- core/include/volume.hpp
+++ core/include/volume.hpp
@@ -14,25 +14,40 @@ struct generic_volume {
 
     virtual std::pair<vec3, vec3> bounding_box() const = 0;
     virtual vec3 size() const = 0;
-    virtual vec3 map_coordinates(vec3 point) const = 0;
+    virtual vec3 map_sampling(const vec3& point) const = 0;
     virtual std::optional<segment> intersection(const segment& previous) const = 0;
 
 };
 
-struct AA_box : generic_volume {
+struct AA_box final : generic_volume {
     vec3 min_point;
     vec3 max_point;
+
+    std::pair<vec3, vec3> bounding_box() const;
+    vec3 size() const;
+    vec3 map_sampling(const vec3& point) const;
+    std::optional<segment> intersection(const segment& previous) const;
 };
 
-struct AA_cylinder : generic_volume {
+struct AA_cylinder final : generic_volume {
     vec3 start_point;
     vec3 end_point;
     double radius;
+
+    std::pair<vec3, vec3> bounding_box() const;
+    vec3 size() const;
+    vec3 map_sampling(const vec3& point) const;
+    std::optional<segment> intersection(const segment& previous) const;
 };
 
-struct AA_ellipsoid : generic_volume {
+struct AA_ellipsoid final : generic_volume {
     vec3 center;
     vec3 radius;
+
+    std::pair<vec3, vec3> bounding_box() const;
+    vec3 size() const;
+    vec3 map_sampling(const vec3& point) const;
+    std::optional<segment> intersection(const segment& previous) const;
 };
 
 bool bounding_box_check(const std::pair<vec3, vec3>& bb, const segment& line);
---
+++ core/src/volume.cpp
@@ -0,0 +1,84 @@
+#include <cmath>
+#include "volume.hpp"
+
+namespace projector {
+
+std::pair<vec3, vec3> AA_box::bounding_box() const {
+    return {min_point, max_point};
+}
+
+vec3 AA_box::size() const {
+    return max_point - min_point;
+}
+
+vec3 AA_box::map_sampling(const vec3& point) const {
+    vec3 scaling_factors = size();
+
+    Eigen::Matrix3d scaling_matrix {
+        {scaling_factors[0], 0, 0},
+        {0, scaling_factors[1], 0},
+        {0, 0, scaling_factors[2]}
+    };
+
+    return min_point + (scaling_matrix * point);
+}
+
+std::optional<segment> AA_box::intersection(const segment& previous) const {
+    return {};
+}
+
+std::pair<vec3, vec3> AA_cylinder::bounding_box() const {
+}
+
+vec3 AA_cylinder::size() const {
+
+}
+
+vec3 AA_cylinder::map_sampling(const vec3& point) const {
+
+}
+
+std::optional<segment> AA_cylinder::intersection(const segment& previous) const {
+    return {};
+}
+
+std::pair<vec3, vec3> AA_ellipsoid::bounding_box() const {
+
+}
+vec3 AA_ellipsoid::size() const {
+    return radius * 2;
+}
+
+
+vec3 AA_ellipsoid::map_sampling(const  vec3& point) const {
+    // TODO: change this mapping to keep the uniformity
+    // take the input as normal vector and normalize it if over length of 1
+    // this does not keep the uniformity -> squishes corner points more
+
+    vec3 sphere_point = (point - vec3{0.5, 0.5, 0.5}) * 2.0;
+
+    double length2 = sphere_point[0] * sphere_point[0]
+                   + sphere_point[1] * sphere_point[1]
+                   + sphere_point[2] * sphere_point[2];
+
+    if (length2 > 1) {
+        sphere_point /= std::sqrt(length2);
+    }
+
+    // scale and translate to volume
+    vec3 scaling_factors = size();
+
+    Eigen::Matrix3d scaling_matrix {
+        {scaling_factors[0], 0, 0},
+        {0, scaling_factors[1], 0},
+        {0, 0, scaling_factors[2]}
+    };
+
+    return center + (scaling_matrix * sphere_point);
+}
+
+std::optional<segment> AA_ellipsoid::intersection(const segment& previous) const {
+    return {};
+}
+
+} //namespace projector
