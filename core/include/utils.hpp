ebfeb2d690184aa1c413c6682d8cb9cfb999faae
Oldrich Pecak <LoaDy588@seznam.cz>
2023-08-03 14:30:10 +0200

    Remove geometry pretty printing utilities

--- core/include/utils.hpp
+++ core/include/utils.hpp
@@ -11,10 +11,5 @@ std::size_t symbol_to_atomic_number(const std::string_view &symbol);
 // utility function for pretty printing nested exceptions
 void print_nested_exception(const std::exception &e, int level = 0);
 
-// pretty printing for geometry
-void print_geometry(const projector::geometry &geom, int level = 0);
-void print_primitive(const projector::geom_primitive &prim, int level = 0);
-void print_operation(const projector::operation &op, int level = 0);
-
 // json loading utility
 nlohmann::json load_json_file(std::filesystem::path);
--- core/src/utils.cpp
+++ core/src/utils.cpp
@@ -4,15 +4,6 @@
 #include <iostream>
 #include <variant>
 
-namespace {
-
-constexpr std::string_view op_type_map[] = {"nop", "join", "intersect", "cut"};
-
-constexpr std::string_view shape_map[] = {"plane", "aa_box", "aa_ellipsoid",
-                                          "aa_cylinder"};
-
-} // namespace
-
 std::size_t symbol_to_atomic_number(const std::string_view &symbol) {
     constexpr std::array<std::string_view, 100> symbols = {
         "H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na", "Mg",
@@ -43,47 +34,6 @@ void print_nested_exception(const std::exception &e, int level) {
         print_nested_exception(next_exception, level + 1);
     } catch (...) {
     }
-}
-
-void print_geometry(const projector::geometry &geom, int level) {
-
-    std::string prefix = std::string(level * 2, ' ');
-    std::cout << prefix << "geometry:" << std::endl;
-
-    std::visit(
-        [level](auto &&arg) {
-            using T = std::decay_t<decltype(arg)>;
-
-            if constexpr (std::is_same_v<T, projector::geom_primitive>) {
-                print_primitive(arg, level + 1);
-            } else if constexpr (std::is_same_v<T, projector::operation>) {
-                print_operation(arg, level + 1);
-            }
-        },
-        geom.definition);
-}
-
-void print_primitive(const projector::geom_primitive &prim, int level) {
-    std::string prefix = std::string(level * 2, ' ');
-
-    std::cout << prefix
-              << "primitive: " << shape_map[static_cast<int>(prim.type)]
-              << std::endl;
-    std::cout << prefix << "param1: " << prim.param1.transpose() << std::endl;
-    std::cout << prefix << "param2: " << prim.param2.transpose() << std::endl;
-}
-
-void print_operation(const projector::operation &op, int level) {
-    std::string prefix = std::string(level * 2, ' ');
-
-    std::cout << prefix << "operation: " << op_type_map[static_cast<int>(op.op)]
-              << std::endl;
-
-    std::cout << prefix << "left:" << std::endl;
-    print_geometry(*op.left, level + 1);
-
-    std::cout << prefix << "right:" << std::endl;
-    print_geometry(*op.right, level + 1);
 }
 
 nlohmann::json load_json_file(std::filesystem::path filepath) {
