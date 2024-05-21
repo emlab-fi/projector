84dc798c2087d0e12b0d9d727ae3105526d6155d
Oldrich Pecak <LoaDy588@seznam.cz>
2024-05-21 16:40:30 -0400

    tooling: add plotting of more data to eprdata14 plotter

--- tooling/plot_eprdata14.py
+++ tooling/plot_eprdata14.py
@@ -37,7 +37,6 @@ def read_ESZG_block(data, N_y):
 
     return energies, inc, coh, pho_el, pair_prod
 
-
 def read_form_factors(data, JXS_2, N_inc, JXS_3, N_coh):
     inc_ff_var = data[JXS_2 - 1 : JXS_2 - 1 + N_inc]
     inc_ff = data[JXS_2 - 1 + N_inc : JXS_2 - 1 + 2 * N_inc]
@@ -46,9 +45,26 @@ def read_form_factors(data, JXS_2, N_inc, JXS_3, N_coh):
     coh_ff_cum = data[JXS_3 - 1 + N_coh : JXS_3 - 1 + 2 * N_coh]
     coh_ff_diff = data[JXS_3 - 1 + 2 * N_coh : JXS_3 - 1 + 3 * N_coh]
 
-    print(coh_ff_cum)
-
     return inc_ff_var, inc_ff, coh_ff_var, coh_ff_cum, coh_ff_diff
+
+def read_subshell_xs(data, JXS_16, N_s, N_y):
+    output = []
+    for i in range(N_s):
+        start = JXS_16 + (i * N_y) - 1
+        stop = start + N_y
+        subshell_raw = data[start : stop]
+        output.append(list(map(unlog_value, subshell_raw)))
+    return output
+
+
+def read_subshell_info(data, JXS_11, N_s):
+    output = []
+    for i in range(5):
+        start = JXS_11 + (i * N_s) - 1
+        stop = start + N_s
+        output.append(data[start : stop])
+    return output
+
 
 def parse_data(header, data):
     converted_data = convert_data(data)
@@ -58,24 +74,54 @@ def parse_data(header, data):
     assert(total_data == len(converted_data))
 
     N_y = int(header[6].split()[2])
+    N_s = int(header[6].split()[6])
 
     JXS_2 = int(header[8].split()[1])
     JXS_3 = int(header[8].split()[2])
     JXS_4 = int(header[8].split()[3])
+    JXS_11 = int(header[9].split()[2])
+    JXS_16 = int(header[9].split()[-1])
 
     N_inc = (JXS_3 - JXS_2) // 2
     N_coh = (JXS_4 - JXS_3) // 3
 
     ESZG_data = read_ESZG_block(converted_data, N_y)
+    subshell_xs = read_subshell_xs(converted_data, JXS_16, N_s, N_y)
     form_factors = read_form_factors(converted_data, JXS_2, N_inc, JXS_3, N_coh)
+    subhsell_info = read_subshell_info(converted_data, JXS_11, N_s)
 
-    return ESZG_data, form_factors
+    return ESZG_data, subshell_xs, form_factors, subhsell_info
 
 
+def print_subshell_info(parsed_data):
+    print("ENDF designator, E. pop., Bind energy, vacancy_prob, transitions")
+    for i in range(len(parsed_data[3][1])):
+        for x in range(5):
+            print(parsed_data[3][x][i],end=" ")
+        print(" ")
+    return
 
-def plot_data(parsed_data):
+def plot_subshell_xs(parsed_data):
     ESZG_data = parsed_data[0]
-    form_factors = parsed_data[1]
+    SPHEL_data = parsed_data[1]
+
+    for i in range(len(SPHEL_data)):
+        plt.plot(ESZG_data[0], SPHEL_data[i], label=f"{i}")
+
+    plt.xlabel("energy (MeV)")
+    plt.ylabel("barns/atom")
+    plt.legend()
+    plt.grid(color='k', linestyle='--', linewidth=0.5)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlim((10**-5, 10**5))
+    plt.show()
+    return
+
+
+def plot_photon_xs(parsed_data):
+    ESZG_data = parsed_data[0]
+    form_factors = parsed_data[2]
 
     fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True, sharey=True)
 
@@ -83,9 +129,10 @@ def plot_data(parsed_data):
     ax1.plot(ESZG_data[0], ESZG_data[2], label='coherent')
     ax1.plot(ESZG_data[0], ESZG_data[3], label='photoeletric')
     ax1.plot(ESZG_data[0], ESZG_data[4], label='pair production')
-    ax1.set_title("Cross sections")
+    ax1.set_title("Total photon cross sections")
     ax1.grid(color='k', linestyle='--', linewidth=0.5)
     ax1.legend()
+    ax1.set_xlim((10**-5, 10**5))
     ax1.set_xlabel("energy (MeV)")
     ax1.set_ylabel("barns/atom")
     ax1.set_yscale('log')
@@ -97,6 +144,7 @@ def plot_data(parsed_data):
     ax2.set_title("Form factors")
     ax2.grid(color='k', linestyle='--', linewidth=0.5)
     ax2.legend()
+    ax2.set_xlim((10**-5, 10**5))
     ax2.set_xlabel("energy (MeV)")
     ax2.set_ylabel("value")
     ax2.set_yscale('log')
@@ -108,8 +156,10 @@ def plot_data(parsed_data):
 
 @click.command()
 @click.option('-e', '--element', 'elem', help="Element to plot", required=True, type=int)
+@click.option('-t', '--type', 'type', help="Type of data to plot",
+              required=True, type=click.Choice(['photon_xs', 'subshell_xs', 'subshell_info']))
 @click.argument('xsdir_path', type=click.Path(exists=True, dir_okay=False, resolve_path=True))
-def main(xsdir_path, elem):
+def main(xsdir_path, elem, type):
     if (elem < 1 or elem > 100):
         print("Invalid element")
         exit()
@@ -133,8 +183,25 @@ def main(xsdir_path, elem):
 
 
     header, data = read_from_file(data_file, int(element_data[5]))
+
+    print("NXS array:")
+    for i in range(6, 8):
+        print(header[i][:-1])
+    print("JXS array:")
+    for i in range(8, 12):
+        print(header[i][:-1])
+
     parsed = parse_data(header, data)
-    plot_data(parsed)
+
+    match type:
+        case 'photon_xs':
+            plot_photon_xs(parsed)
+        case 'subshell_xs':
+            plot_subshell_xs(parsed)
+        case 'subshell_info':
+            print_subshell_info(parsed)
+        case _:
+            print("unsupported plot type")
 
 
 
